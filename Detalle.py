4163d1536e9941db4938fb5d3683a20c75c3ad11
César Nuñez <cesar.nunez@gbrx.com>
2025-07-29 11:33:45 -0600

    Update Detalle.py

--- Detalle.py
+++ Detalle.py
@@ -16,8 +16,6 @@ def main():
     st.markdown(f"**Descripción:** {descripcion}")
     st.markdown(f"**Ubicación:** {ubicacion}")
 
-    st.markdown("---")
-    st.page_link("main.py", label="⬅️ Volver al Inventario", icon="📦")
 
 if __name__ == "__main__":
     main()
